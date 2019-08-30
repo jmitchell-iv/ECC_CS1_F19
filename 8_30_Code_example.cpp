eb1797e8c8c62486f53f6889f9b6ec17a78703f9
aanoguchi <54706068+aanoguchi@users.noreply.github.com>
2019-08-30 15:49:23 -0700

    Create 8_30_Code_example.cpp

---
+++ 8_30_Code_example.cpp
@@ -0,0 +1,10 @@
+int main()
+{
+	// this is a comment
+	double f =1.0;
+	string s = "14";
+	double mpg = 39.5;
+	int t = 100 + 1000;
+	cout << 3 << "\n" << "Hello World timmy!\n" << i << "\n";
+	cout << "\\";
+}
