04bdbb3664aa87c08c475785cbce7ca4189a2141
aanoguchi <54706068+aanoguchi@users.noreply.github.com>
2019-09-06 09:48:48 -0700

    added the demo from the first lab of 9/6

---
+++ Code_Demos/9_6_First_section.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Purpose
+    // Make two variables, 50 and 100 and store the sum
+    // in a third variable. Display the value of the
+    // third variable.
+
+    // Make a variable and store 50 in it.
+    int x = 50;
+
+    // Make another variable and store 100 in it.
+    int a = 100;
+
+    // Make a third variable and store the sum of the
+    // first two in it.
+    int sum = a + x;
+
+    // Print the third variable in the console.
+    cout << "The sum of " 
+         << x << " and " << a << " is " 
+         << sum << "\n";
+    return 0;
+}
