37d9b607b8c7a42b71a1bb7ad2ad62c24048a7f1
aanoguchi <54706068+aanoguchi@users.noreply.github.com>
2019-09-13 01:08:11 -0700

    quiz with skeleton

---
+++ Quizzes/Q3.cpp
@@ -0,0 +1,15 @@
+/*
+ * Quiz 3
+ * Write a program which asks the user for 5 numbers,
+ * then calculates and outputs the average of those numbers.
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
