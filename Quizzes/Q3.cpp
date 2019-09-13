4e0d894dcf47bdf85783db77612336f0b3276bdd
aanoguchi <54706068+aanoguchi@users.noreply.github.com>
2019-09-13 10:55:34 -0700

    Added quiz 3, lecture notes, and a code rubric.

--- Quizzes/Q3.cpp
+++ Quizzes/Q3.cpp
@@ -4,12 +4,18 @@
  * then calculates and outputs the average of those numbers.
  * 
  */
-
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    
+    // Initialize variables
+    // Ask user for 5 numbers
+    // Store numbers as variables
+    // Calculate average
+        // Find sum of numbers
+        // Divide by 5
+        // Store the average
+    // Display the average
     return 0;
 }
