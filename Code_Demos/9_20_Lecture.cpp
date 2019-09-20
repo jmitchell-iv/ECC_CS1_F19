4f7edac4ac22a0ac31d0f6acdec201b5e8fede1d
aanoguchi <54706068+aanoguchi@users.noreply.github.com>
2019-09-20 10:48:53 -0700

    Quiz for and Lecture demo on loop invariants

---
+++ Code_Demos/9_20_Lecture.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Countdown to launch
+  int c = 18;
+  while(c >= 0) {
+    cout << c  << endl;
+    c = c - 1;
+  }
+  cout << "Ignition!!!1!!111" << endl;
+  return 0;
+}
---
+++ Quizzes/Q4.cpp
@@ -0,0 +1,26 @@
+/*
+ * Quiz 4
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  // Question 1 [T/F]
+  // In this class we are using one line if statements that don't need a block
+  // Question 2 [Substitution Model]
+  int precidence = 8 / 2 * (2 + 2) % 2;
+  // Question 3 [Evaluate]
+  //  What does the following output?
+   int yes = 42;
+   if (yes = 24)
+     cout << "Cats eat bats";
+     cout << "Contrawise";
+   // Question 4 [Code]
+   // Give the pseodocode for the following program:
+   // As the user for the dimension of two rectangles, tell the user which has a
+   // greater area, or if they are the same.
+     return 0;
+}
